0e5ad9339c024e83cfa64aa29a0a9734c47eba68
zhangzhi <zhangzhihtz@gmail.com>
2023-05-19 14:24:19 +0800

    使用 singleflight 防止缓存击穿，实现与测试

--- gocache/gocache.go
+++ gocache/gocache.go
@@ -2,6 +2,7 @@ package gocache
 
 import (
 	"fmt"
+	"go-cache/singleflight"
 	"log"
 	"sync"
 )
@@ -13,6 +14,9 @@ type Group struct {
 	getter    Getter // 缓存未命中时获取源数据的回调
 	mainCache cache  // 并发缓存
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -32,6 +36,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 	groups[name] = g
 	return g
@@ -72,15 +77,23 @@ func (g *Group) Get(key string) (ByteView, error) {
 // 使用 PickPeer() 方法选择节点，若非本机节点, 则调用 getFromPeer() 从远程获取
 // 若是本机节点或失败, 则回退到 getLocally()
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
 			}
-			log.Println("[GeeCache] Failed to get from peer", err)
 		}
+		return g.getLocally(key)
+	})
+	if err == nil {
+		return viewi.(ByteView), nil
 	}
-	return g.getLocally(key)
+	return
 }
 
 // 使用实现了 PeerGetter 接口的 httpGetter 从访问远程节点, 获取缓存值
